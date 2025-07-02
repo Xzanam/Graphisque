52cbaa7e8b1630fc3455c23073d1a69195387153
Janam <sznam77@gmail.com>
2025-07-02 22:06:11 +0545

    Fixes

--- include/Graphisque/Shader.h
+++ include/Graphisque/Shader.h
@@ -8,14 +8,16 @@ class Shader
 {
 public:
     unsigned int ID;
+    Shader() = default;
     Shader(const char *vertexPath, const char *fragmentPath);
     // Shader(const char *vertexPath, const char *includePath, const char *fragmentPath);
 
-    void use();
+    void use() const ;
 
     void setBool(const std::string &name, bool value) const;
     void setFloat(const std::string &name, float value) const;
     void setVec3(const std::string &name, const glm::vec3 &value) const;
+    void setVec4(const std::string &name, const glm::vec4 &value) const;
     void setMat4(const std::string &name, const glm::mat4 &mat) const;
     void setInt(const std::string &name, int value) const;
 };
