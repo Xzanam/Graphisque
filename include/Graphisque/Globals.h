5859d1522d2b7ef5fbe77d21716045235cf65f8d
Janam Shrestha <sznam77@gmail.com>
2025-07-09 00:41:53 +0545

    Global Constants in single file

--- include/Graphisque/Globals.h
+++ include/Graphisque/Globals.h
@@ -1,8 +1,9 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
-extern const int WinWidth;  // Default window width
-extern const int WinHeight; // Default window height
+extern const int G_WinWidth;  // Default window width
+
+extern const int G_WinHeight; // Default window height
 
 
 #endif // GLOBALS_H
