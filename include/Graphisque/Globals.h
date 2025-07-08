26770a6727a1c1b38a1a821ee0d10e6fc7a72ec8
Janam Shrestha <sznam77@gmail.com>
2025-07-08 23:57:28 +0545

    Add Globals.h for global variables

---
+++ include/Graphisque/Globals.h
@@ -0,0 +1,8 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+extern const int WinWidth;  // Default window width
+extern const int WinHeight; // Default window height
+
+
+#endif // GLOBALS_H
---
+++ src/Globals.cpp
@@ -0,0 +1,4 @@
+#include "Globals.h"
+
+const int WinWidth = 800;  // Default window width
+const int WinHeight = 600; // Default window height 
