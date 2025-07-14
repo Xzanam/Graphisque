0497581600931bdd64427ecfb2b5957979d836b9
Janam Shrestha <sznam77@gmail.com>
2025-07-15 00:45:46 +0545

    File for Global Constants

--- src/Globals.cpp
+++ src/Globals.cpp
@@ -1,4 +1,5 @@
 #include "Globals.h"
 
 const int G_WinWidth = 800;  // Default window width
-const int G_WinHeight = 600; // Default window height 
+const int G_WinHeight = 600; // Default window height
+
